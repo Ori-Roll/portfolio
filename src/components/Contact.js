4620e1a6e7fdff5c885291501503a95b3f2a8209
Ori Roll <oriroll@gmail.com>
2020-03-04 14:29:35 +0200

    Contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Contact() {
-	return <div className='contact-page'>THIS</div>;
+	return (
+		<div className='contact-page'>
+			<h3 className='cv-section-header'>CONTACT ME</h3>
+
+			<h4>My email:</h4>
+			<a href='mailto:oriroll@gmail.com'>oriroll@gmail.com</a>
+			<br />
+			<br />
+			<h4>Github page:</h4>
+			<a href='https://github.com/Ori-Roll'>https://github.com/Ori-Roll</a>
+			<br />
+			<br />
+			<h4>Phone number:</h4>
+			<h4 style={{ fontSize: "1em" }}>054-4897787</h4>
+		</div>
+	);
 }
 
 export default Contact;
