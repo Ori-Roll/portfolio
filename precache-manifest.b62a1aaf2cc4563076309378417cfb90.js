a7caa057218d690637ac923a48e2dd2a82c742c8
Ori Roll <oriroll@gmail.com>
2021-02-01 13:17:37 +0200

    Updates

---
+++ precache-manifest.b62a1aaf2cc4563076309378417cfb90.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405e519e5fed5a22e4d445cd7fbc73a0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "12a623aa44f9b4395aa3",
+    "url": "/portfolio/static/css/main.2615f68d.chunk.css"
+  },
+  {
+    "revision": "1df0b95e9b683fd69069",
+    "url": "/portfolio/static/js/2.dcd4fbb9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.dcd4fbb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a623aa44f9b4395aa3",
+    "url": "/portfolio/static/js/main.870cebbe.chunk.js"
+  },
+  {
+    "revision": "1ce76c02ee4034b846c7",
+    "url": "/portfolio/static/js/runtime-main.19a89414.js"
+  },
+  {
+    "revision": "d66e7acd929e8122eda4a1aa588be19f",
+    "url": "/portfolio/static/media/Lister.d66e7acd.png"
+  },
+  {
+    "revision": "63eaf8548dd4d91dbc5a0b30dd79caa2",
+    "url": "/portfolio/static/media/Ori-Cv.63eaf854.pdf"
+  },
+  {
+    "revision": "372dbd70a72bad454871eccec40154f4",
+    "url": "/portfolio/static/media/avatar.372dbd70.png"
+  },
+  {
+    "revision": "2520161d565bafb74098fedb6c737f56",
+    "url": "/portfolio/static/media/calc.2520161d.png"
+  },
+  {
+    "revision": "d713b8cfa61a47a85ae693e80d662550",
+    "url": "/portfolio/static/media/calcal.d713b8cf.gif"
+  },
+  {
+    "revision": "ab5b578575b939e27cd9045218eee685",
+    "url": "/portfolio/static/media/maps.ab5b5785.gif"
+  },
+  {
+    "revision": "9d2d02b045dcf1686552ad1aa0dfa92f",
+    "url": "/portfolio/static/media/p-m-gif.9d2d02b0.gif"
+  },
+  {
+    "revision": "64170880b2b4d64a6e486908d41575f7",
+    "url": "/portfolio/static/media/pdf17.64170880.svg"
+  }
+]);
