48be03080ad0b7bf9bc45b0471b9ccef3309a84d
Ori Roll <oriroll@gmail.com>
2022-07-17 13:28:14 +0300

    Moved cv data to separate object

---
+++ src/data/cvData.js
@@ -0,0 +1,99 @@
+export const cvData = {
+  about:
+    "Hey, I'm Ori, a Junior web developer (also, formerly a building architect - b.arch from the Technion). I was writing code most of my life and am very passionet about it. I like simple and efficient solutions for programming problems and always try having reusable and concise code. I've created websites, games, a simple project managment app (like Monday.com) a calendar app for my portfolio (like google calendar) and more.",
+  work: [
+    {
+      id: 1,
+      workplace:
+        "Working on a lean project management web app startup",
+      year: "~2020",
+      jobTitle: null,
+      details:
+        "During the corona outbreak, I worked on a web app startup - a proof of concept for a project managment app (similar to monday.com or clickUp). I designed all of the client side structure (with React) and a most the server side operations (based on mongoDB and using node +express). I was working along with experienced developers.",
+    },
+    { id: 2, commentHeader: "Older non-FED work:" },
+    {
+      id: 3,
+      workplace: "Milner architects",
+      year: "2016 - 2019",
+      jobTitle: "Staff architect",
+      details:
+        "Private and commercial projects such as hotels and health clubs. Worked with and managing planning consultant teams and helping with supervising the building process until project completion.",
+    },
+    {
+      id: 4,
+      workplace: "Independent work (under contract for Azrieli)",
+      year: "2015 - 2016",
+      jobTitle: "Architect",
+      details:
+        "Assisting Azrieli’s head architect with internal projects. Working in conjunction with external architects and building consultants.",
+    },
+    {
+      id: 5,
+      workplace: "Nahmias architects",
+      year: "2014 - 2015",
+      jobTitle: "Junior staff architect",
+      details: "Working on many architectural projects.",
+    },
+  ],
+
+  skills: [
+    {
+      id: "1",
+      level: "3",
+      skill: "HTML, CSS",
+      comment: "",
+    },
+    {
+      id: "2",
+      level: "4",
+      skill: "JavaScript",
+      comment:
+        "Very good understanding of concepts like functional programming, state, closures, scope, asynchronous, etc.",
+    },
+    {
+      id: "3",
+      level: "4",
+      skill: "React.js",
+      comment:
+        "(with redux/contextAPI, react router). Proficient with function components, structure and render efficiancy.",
+    },
+    {
+      id: "4",
+      level: "1.5",
+      skill: "Node.js + express + mongoDB+mongoose",
+      comment: "My server-side knowledge is relatively basic",
+    },
+  ],
+  contact: [
+    {
+      id: "1",
+      title: "Phone:",
+      textData: "054-4897787",
+    },
+    {
+      id: "2",
+      title: "Email:",
+      linkData: "oriroll@gmail.com",
+      linkRef: "mailto:oriroll@gmail.com",
+    },
+    {
+      id: "3",
+      title: "GitHub page",
+      linkRef: "https://github.com/Ori-Roll",
+      linkData: "github.com/Ori-Roll",
+    },
+    {
+      id: "4",
+      title: "Portfolio:",
+      linkRef: "https://ori-roll.github.io/portfolio/#/",
+      linkData: "ori-roll.github.io/portfolio",
+    },
+    {
+      id: "5",
+      title: "Linkedin:",
+      linkRef: "https://www.linkedin.com/in/ori-roll-11152a1a7/",
+      linkData: "linkedin.com/ori-roll",
+    },
+  ],
+};
