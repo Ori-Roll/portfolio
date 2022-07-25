0aa5caf65a0abda37ab3e4d238270dd4c6f962e4
Ori Roll <oriroll@gmail.com>
2022-07-25 17:32:17 +0300

    Episode - quick fix - values as optional - responsibilities - more new design - no cleanup!

--- src/components/Cv/Episode.js
+++ src/components/Cv/Episode.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Episode = (props) => {
-  const { workplace, year, jobTitle, details } = props;
+  const { workplace, year, jobTitle, details, responsibilities } =
+    props;
 
   return (
     <div className="cv-episode">
-      <div className="cv-episode-header">
-        <h2>{workplace}</h2>
-        <h4>{year}</h4>
-      </div>
-      {jobTitle ? <h3>{jobTitle}</h3> : null}
+      {jobTitle || workplace || year ? (
+        <div className="cv-episode-header">
+          {jobTitle ? (
+            <>
+              <h2>{jobTitle}</h2>
+              <small>{`/`}</small>
+            </>
+          ) : null}
+          {workplace ? <h2>{workplace}</h2> : null}
+          {year ? (
+            <>
+              <small>{`/`}</small>
+              <h2>{year}</h2>
+            </>
+          ) : null}
+        </div>
+      ) : null}
+
       <p>{details}</p>
+      {responsibilities ? (
+        <ul className="cv-indented-ul">
+          {responsibilities.map((responsibility) => (
+            <li className="cv-indented-li">{responsibility}</li>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 };
