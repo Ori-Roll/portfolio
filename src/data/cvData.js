363aef73b1cd37ac04f7da82d2934160657bf154
Ori Roll <oriroll@gmail.com>
2022-07-25 17:30:41 +0300

    cv data update

--- src/data/cvData.js
+++ src/data/cvData.js
@@ -1,39 +1,45 @@
 export const cvData = {
   about:
-    "Hey, I'm Ori, a Junior web developer (also, formerly a building architect - b.arch from the Technion). I was writing code most of my life and am very passionet about it. I like simple and efficient solutions for programming problems and always try having reusable and concise code. I've created websites, games, a simple project managment app (like Monday.com) a calendar app for my portfolio (like google calendar) and more.",
+    "Capable front-end web developer, highly adept at finding simple and efficient solutions for programming problems and creating performant, reusable and concise code. Proficient with Javascript, Typescript, React, CSS, HTML and more. (also, formerly a building architect - b.arch from the Technion)",
   work: [
     {
       id: 1,
+      workplace: "BioT medical",
+      year: "2021 - Aug 2022",
+      jobTitle: "Front end developer",
+      details:
+        "Working as part of a FE team to create the company's new core 2'nd gen system, an off-the-shelf platform for connecting medical devices. We built 3 apps, including a UI-data-structure builder app, and a layer-designed libraries monorepo (managed with NX).\nThe apps utilize user customizable, entities data-structure, with generic code patterns and modular UI building blocks, to form the UI structure, that is then \"fed\" values from medical devices or any other source.",
+      responsibilities: [
+        "Created the apps' generic, data-aware, display & input renderers system, file upload system, multiple smart input components and form items, wrapping intl mechanism, and much more.",
+        "Diagnosed system wide unmount issues, leading to a refactor greatly improving performance and stability.",
+        "Technologies used: Typescript (including advanced TS patterns), MUI, Styled-components, Formik, Yup validations, react-intl internationalization. Occasionally Jest, Redux (w/ toolkit) + Saga.\nUsed Node.js for scripts and AWS lambdas",
+      ],
+    },
+    {
+      id: 2,
       workplace:
         "Working on a lean project management web app startup",
       year: "~2020",
-      jobTitle: null,
       details:
-        "During the corona outbreak, I worked on a web app startup - a proof of concept for a project managment app (similar to monday.com or clickUp). I designed all of the client side structure (with React) and a most the server side operations (based on mongoDB and using node +express). I was working along with experienced developers.",
+        "During the corona outbreak, worked on a web app startup - a proof of concept for a project management app (like monday.com or clickUp). Designed most of the client side structure (with React) and some of the server side operations (node + mongoDB + express). Worked along with experienced developers.",
     },
-    { id: 2, commentHeader: "Older non-FED work:" },
     {
       id: 3,
-      workplace: "Milner architects",
-      year: "2016 - 2019",
-      jobTitle: "Staff architect",
+      workplace: "Other Architecture Work",
+      year: "2014 - 2019",
       details:
-        "Private and commercial projects such as hotels and health clubs. Worked with and managing planning consultant teams and helping with supervising the building process until project completion.",
+        "Before 2020 I worked at 3 architecture firms - Milner architects (2016 - 2019), Under contract for Azrieli (2015 - 2016), and Nahmias architects (2014 - 2015).",
     },
-    {
-      id: 4,
-      workplace: "Independent work (under contract for Azrieli)",
-      year: "2015 - 2016",
-      jobTitle: "Architect",
-      details:
-        "Assisting Azrieli’s head architect with internal projects. Working in conjunction with external architects and building consultants.",
-    },
+    { id: 4, commentHeader: "EDUCATION AND OTHER BACKGROUND" },
     {
       id: 5,
-      workplace: "Nahmias architects",
-      year: "2014 - 2015",
-      jobTitle: "Junior staff architect",
-      details: "Working on many architectural projects.",
+      details:
+        "2013 / Bachelor's degree in architecture from Technion (B.Arc).",
+    },
+    {
+      id: 6,
+      details:
+        "2003 / Medic in the Combat engineering corps (“Handasa kravit”)",
     },
   ],
 
@@ -42,27 +48,56 @@ export const cvData = {
       id: "1",
       level: "3",
       skill: "HTML, CSS",
-      comment: "",
+      comment:
+        "Worked with vanilla CSS, Styled-components and CSS-classes",
     },
     {
       id: "2",
       level: "4",
       skill: "JavaScript",
       comment:
-        "Very good understanding of concepts like functional programming, state, closures, scope, asynchronous, etc.",
+        "Very good understanding of concepts like functional programming, state, closures, scope, asynchronous and promises, event loop and more.",
     },
     {
       id: "3",
       level: "4",
-      skill: "React.js",
+      skill: "Typescript",
       comment:
-        "(with redux/contextAPI, react router). Proficient with function components, structure and render efficiancy.",
+        "Including generics, conditional types, utility types, enums amd more.",
     },
     {
       id: "4",
+      level: "4",
+      skill: "React.js",
+      comment:
+        "(With redux/contextAPI, react router). Proficient with function components, hooks, structure and render efficiency.",
+    },
+    {
+      id: "5",
+      level: "4",
+      skill: "Libs",
+      comment:
+        "Libraries include MUI (Material UI), React-Formik with Yup-validations, Storybook, Jest, and others.",
+    },
+    {
+      id: "6",
       level: "1.5",
-      skill: "Node.js + express + mongoDB+mongoose",
+      skill: "Node.js",
       comment: "My server-side knowledge is relatively basic",
+    },
+    {
+      id: "7",
+      level: "4",
+      skill: "Other sources",
+      comment:
+        "VSCode, npm, git. \n AutoCAD, Revit, 3ds-max, Photoshop",
+    },
+    {
+      id: "8",
+      level: "4",
+      skill: "Language skills",
+      comment:
+        "Hebrew - native speaker. \n English - native speaker level",
     },
   ],
   contact: [
@@ -70,30 +105,35 @@ export const cvData = {
       id: "1",
       title: "Phone:",
       textData: "054-4897787",
+      iconName: "phone",
     },
     {
       id: "2",
       title: "Email:",
       linkData: "oriroll@gmail.com",
       linkRef: "mailto:oriroll@gmail.com",
+      iconName: "email",
     },
     {
       id: "3",
       title: "GitHub page",
       linkRef: "https://github.com/Ori-Roll",
       linkData: "github.com/Ori-Roll",
+      iconName: "github",
     },
     {
       id: "4",
       title: "Portfolio:",
       linkRef: "https://ori-roll.github.io/portfolio/#/",
       linkData: "ori-roll.github.io/portfolio",
+      iconName: "portfolio",
     },
     {
       id: "5",
       title: "Linkedin:",
       linkRef: "https://www.linkedin.com/in/ori-roll-11152a1a7/",
       linkData: "linkedin.com/ori-roll",
+      iconName: "linkedin",
     },
   ],
 };
