14f0e6deb1edba0ad785fdfc0896542d7e0b62a9
Ori Roll <oriroll@gmail.com>
2022-07-25 17:51:49 +0300

    New Contact comp update

--- src/components/Cv/Contact.js
+++ src/components/Cv/Contact.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
+import { iconNames, iconsMap } from "./../../utils/mapIconToName";
 
 const Contact = (props) => {
-  const { title, linkData, linkRef, textData } = props;
-  console.log('props are ', props);
+  const { title, linkData, linkRef, textData, iconName } = props;
+  console.log("props are ", props);
+
+  console.log(iconNames);
+  console.log(iconName);
+  console.log(iconsMap);
+
+  const ContactIcon = iconsMap.get(iconNames[iconName]);
 
   return (
-    <div>
-      {title ? <h4>{title}</h4> : null}
+    <div className="cv-section-contact-deatils">
+      {/* {title ? <h4>{title}</h4> : null} */}
+      {iconName ? (
+        <div className="cv-small-icon-wrapper">
+          <ContactIcon />
+        </div>
+      ) : null}
       {linkData && linkRef ? <a href={linkRef}>{linkData}</a> : null}
       {textData ? <p>{textData}</p> : null}
     </div>
